1b2cab6c85d2ae3758de23364910ddd1f6800af6
chensiyou <905630790@qq.com>
2019-04-28 18:38:41 +0800

    feat:"框架搭建"

--- spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/csy/oauth2/OAuth2ServerApplication.java
+++ spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/csy/oauth2/OAuth2ServerApplication.java
@@ -3,6 +3,7 @@ package com.csy.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OAuth2ServerApplication {
     public static void main(String[] args) {
